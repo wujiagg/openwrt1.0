63bb8c69d85dbd939646cd79969dd45142e9ec49
wujiagg <63491024+wujiagg@users.noreply.github.com>
2020-09-29 12:18:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,4 @@
 #添加软件包
 git clone https://github.com/maxlicheng/luci-app-ssr-plus.git package/luci-app-ssr-plus
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
-git clone https://github.com/liuran001/openwrt-packages package
+git clone https://github.com/liuran001/openwrt-packages package/openwrt-packages
