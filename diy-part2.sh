d2700412395ce408f970ca75b0ae3ca12c2d57d2
wujiagg <63491024+wujiagg@users.noreply.github.com>
2021-02-24 11:05:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #添加软件包
-git clone https://github.com/maxlicheng/luci-app-ssr-plus.git package/luci-app-ssr-plus
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/liuran001/openwrt-packages package/openwrt-packages
